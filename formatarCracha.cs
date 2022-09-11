c85abfd7e3ec19cb158dfa2f49a65610b4c7e2e7
Ellen Rocha <ellen_rochafarias@hotmail.com>
2022-09-11 17:50:36 -0300

    Realização da aplicação

---
+++ formatarCracha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solucao03_Cracha
+{
+    class formatarCracha
+    {
+        private static String nome;
+
+        public static void setNome(String _nome) { nome = _nome; }
+        public static String getNome() { return nome; }
+
+        public static int getUltimoEspaco()
+        {
+            int i;
+            for (i = nome.Length - 1; nome[i] != ' '; --i) ;
+            return i;
+        }
+
+        public static String getUltimoSobrenome()
+        {
+            return nome.Substring(getUltimoEspaco() + 1, nome.Length - getUltimoEspaco() - 1).ToUpper();
+        }
+
+        public static String getIniciais()
+        {
+            String tam = nome.Substring(0, getUltimoEspaco());
+            String aux = "";
+            for(int l = 0; l < tam.Length; l++) {
+            if(char.IsUpper(tam[l]) == true) {
+                    aux += tam[l] + ". ";
+                }
+            };
+                return aux;
+        }
+    }
+}
